716af5d1c3ab2b5a07a16dd343e4a73de05f6917
Joel Jose <joeljose98953@gmail.com>
2026-09-06 06:29:46 +0530

    Fixed routing for github

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,24 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import { copyFileSync, existsSync, writeFileSync } from 'fs'
+import { resolve } from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: 'github-pages-spa',
+      closeBundle() {
+        const distIndex = resolve('dist/index.html')
+        const dist404 = resolve('dist/404.html')
+        const distNoJekyll = resolve('dist/.nojekyll')
+        if (existsSync(distIndex)) {
+          copyFileSync(distIndex, dist404)
+        }
+        writeFileSync(distNoJekyll, '')
+      },
+    },
+  ],
   base: '/useless_dumb_mart/',
 })
